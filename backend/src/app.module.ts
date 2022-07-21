c9f374a9e286eca08e58c4aad6aa187d8e3d8bc1
Daniel Vidal <danielsv465@hotmail.com>
2022-07-20 22:34:57 -0300

    Iniciando projeto

---
+++ backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// import { WarehouseModule } from './warehouse/warehouse.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      name: 'default',
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'almoxarifado',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/database/migrations/*{.ts,.js}'],
+      schema: 'almoxarifado',
+      cli: {
+        migrationsDir: './src/database/migrations'
+      }
+    })
+  ]
+})
+export class AppModule {}
